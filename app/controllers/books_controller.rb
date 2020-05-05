15ccad50137531c415b722b6234efe5709c4e168
vagrant <vagrant@localhost.localdomain>
2020-05-05 13:13:15 +0000

    [Update]5/5までの作業分

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,11 +30,11 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path, notice: "Book was successfully destroyed."
+    redirect_to books_path
   end
 
   private
   def book_params
   params.require(:book).permit(:title, :body)
-end
+  end
 end
