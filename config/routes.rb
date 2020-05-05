9c225e2380f61d94706bebce15052835ce48b1ef
vagrant <vagrant@localhost.localdomain>
2020-05-05 08:31:45 +0000

    [Update]大枠できた！ 

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root :to => 'books#index'
 get 'books' => 'books#new', as: 'books'
-post 'todolists' => 'todolists#create'
 resources :books
 end
