83697fdc4969dde46a5d10ff09964a58ae165eee
vagrant <vagrant@localhost.localdomain>
2020-05-06 00:35:35 +0000

    [Update]あとはデザインを整えるだけ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,13 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to books_path
+    else
+      flash[:notice] = "Failed to create."
+      redirect_to books_path
+    end
   end
 
   def edit
@@ -23,14 +28,19 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
     redirect_to book_path(book)
+    else
+      flash[:notice] = "Failed to update."
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
   private
